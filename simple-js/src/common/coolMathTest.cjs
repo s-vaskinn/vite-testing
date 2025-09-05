5dbf681bbe17d32bacb7b62db58726e3c98db184
Sindre Vaskinn <sindre.vaskinn@samspar.no>
2025-09-05 13:14:42 +0000

    es and common js

---
+++ simple-js/src/common/coolMathTest.cjs
@@ -0,0 +1,2 @@
+const add = require('./coolMath.cjs')
+console.log(add(2, 3)) // 5
---
+++ simple-js/src/esm/coolMathTest.js
@@ -0,0 +1,3 @@
+//const add = require('./coolMath.js')
+import { add } from './coolMath.js'
+console.log(add(2, 4)) // 6
