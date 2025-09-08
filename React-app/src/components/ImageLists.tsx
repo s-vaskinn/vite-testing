a5721a216db9fd08301956ca8d3896bb984e21b8
Sindre Vaskinn <sindre.vaskinn@samspar.no>
2025-09-08 06:17:25 +0000

    used glob in App

--- React-app/src/components/ImageLists.tsx
+++ React-app/src/components/ImageLists.tsx
@@ -1,4 +1,4 @@
-const images = import.meta.glob('../assets/london/{*.jpg,*.png,*.svg}', { eager: true })
+const images = import.meta.glob<{ default: string }>('../assets/london/{*.jpg,*.png,*.svg}', { eager: true })
 
 console.log(images)
 
@@ -7,6 +7,11 @@ export function ImageLists() {
     <div>
       <h2>Image Lists</h2>
       <p>This is the Image Lists component.</p>
+      {Object.values(images).map(
+        path => <img src={path.default} 
+        key={path.default} 
+        width='400' />
+      )}
     </div>
   )
 }
