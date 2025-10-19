5c4ed53666b7fd259211411f6a10625d89b4b5b7
Sindre Vaskinn <sindre.vaskinn@samspar.no>
2025-10-19 18:53:02 +0000

    adding routes

--- crypto-dash/src/App.jsx
+++ crypto-dash/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect} from 'react';
-import CoinCard from './components/CoinCard';
-import LimitSelector from './components/LimitSelector';
-import FilterInput from './components/FilterInput';
-import SortSelector from './components/SortSelector';
+import HomePage from './pages/home';
+import { Routes, Route } from 'react-router';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -32,60 +30,27 @@ const App = () => {
         setLoading(false);
       }
     }
-
     fetchCoins();
   }, [limit]);
 
-  const filteredCoins = coins.filter(coin => {
-    return coin.name.toLowerCase().includes(filter.toLowerCase()) || 
-    coin.symbol.toLowerCase().includes(filter.toLowerCase())
-  })
-  .slice()
-  .sort((a,b) => {
-    switch(sortBy) {
-      case 'market_cap_desc':
-        return b.market_cap - a.market_cap;
-      case 'market_cap_asc':
-        return a.market_cap - b.market_cap;
-      case 'price_desc':
-        return b.current_price - a.current_price;
-      case 'price_asc':
-        return a.current_price - b.current_price;
-      case 'change_desc':
-        return b.price_change_percentage_24h - a.price_change_percentage_24h;
-      case 'change_asc':
-        return a.price_change_percentage_24h - b.price_change_percentage_24h;
-      case 'volume_desc':
-        return b.total_volume - a.total_volume;
-      case 'volume_asc':
-        return a.total_volume - b.total_volume;
-      default:
-        return 0;
-    }
-  })
+
 
   return (
-    <div>
-      <h1> Welcome to Crypto Dash</h1>
-
-      { loading && <p>Loading...</p> }
-      { error && <div className='error'>{error}</div> }
-      <div className="top-controls">
-        <FilterInput filter={filter} onFilterChange={setFilter} />
-        <LimitSelector limit={limit} onLimitChange={setLimit} />
-        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-      </div>
-      {!loading && !error && (
-        <main className='grid'>
-          {filteredCoins.length > 0 ? (
-            filteredCoins.map(coin => (
-              <CoinCard key={coin.id} coin={coin} />
-            ))
-            ) : ( <p>No coins found</p> )
-          }
-        </main>
-      )}
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <HomePage 
+          coins={coins}
+          filter={filter}
+          setFilter={setFilter}
+          limit={limit}
+          setLimit={setLimit}
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+          loading={loading}
+          error={error}
+         />
+      } />
+    </Routes>
   );
 };
 
